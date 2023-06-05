8b5fd2de3efdc096c6afe89863d4debaf7e22f1d
Eri Schon <erischon@gmail.com>
2023-06-05 08:34:24 +0200

    Update CreateEmployee-page.jsx

--- src/pages/CreateEmployee-page.jsx
+++ src/pages/CreateEmployee-page.jsx
@@ -3,8 +3,10 @@ import CreateEmployeeForm from "../components/CreateEmployeeForm";
 function CreateEmployee() {
   return (
     <>
-      <div className="">
-        <h2>Create Employee</h2>
+      <div className="flew w-full ">
+        <h2 className="text-2xl font-semibold text-center mb-4">
+          Create Employee
+        </h2>
 
         <CreateEmployeeForm />
       </div>
